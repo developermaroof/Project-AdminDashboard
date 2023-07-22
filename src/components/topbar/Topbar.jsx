6afa4e71d4aececf76959c2a93f1ae7576f60a95
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-22 20:23:21 +0500

    Added initial files

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./topbar.css";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">Left</div>
+        <div className="topRight">Right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
