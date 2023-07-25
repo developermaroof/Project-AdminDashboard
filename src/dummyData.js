067ef71874d90e03aa634ff46adda91f0770c82d
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-25 16:02:14 +0500

    Updated

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 3000,
+  },
+  {
+    name: "Mar",
+    "Active User": 2000,
+  },
+  {
+    name: "Apr",
+    "Active User": 5000,
+  },
+  {
+    name: "May",
+    "Active User": 6000,
+  },
+  {
+    name: "Jun",
+    "Active User": 7000,
+  },
+  {
+    name: "Jul",
+    "Active User": 1000,
+  },
+  {
+    name: "Aug",
+    "Active User": 4000,
+  },
+  {
+    name: "Sep",
+    "Active User": 3000,
+  },
+  {
+    name: "Oct",
+    "Active User": 2000,
+  },
+  {
+    name: "Nov",
+    "Active User": 3000,
+  },
+  {
+    name: "Dec",
+    "Active User": 5000,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets"></div>
     </div>
   );
 };
