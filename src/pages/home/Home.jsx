ff95211e2a49036b22262bd2c046dfdb840b32d0
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-25 21:07:24 +0500

    Updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 const Home = () => {
   return (
@@ -13,7 +15,10 @@ const Home = () => {
         grid
         dataKey="Active User"
       />
-      <div className="homeWidgets"></div>
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 };
---
+++ src/pages/userList/UserList.jsx
@@ -0,0 +1,7 @@
+import "./userList.css";
+
+const UserList = () => {
+  return <div className="userList">UserList</div>;
+};
+
+export default UserList;
